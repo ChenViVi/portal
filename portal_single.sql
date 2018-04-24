7d254c0ffd76a2483027e1513f67e223572142b3
ChenViVi <932942491@qq.com>
2018-04-24 15:03:25 +0800

    数据库文件去掉不用的表

--- portal_single.sql
+++ portal_single.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.0.10.20
+-- https://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: 2018-04-08 10:03:06
--- 服务器版本： 5.7.14
--- PHP Version: 5.6.25
+-- 主机: 127.0.0.1
+-- 生成日期: 2018-04-24 07:02:13
+-- 服务器版本: 5.7.21
+-- PHP 版本: 5.6.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,10 +14,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `portal_single`
+-- 数据库: `portal_single`
 --
 
 -- --------------------------------------------------------
@@ -26,10 +26,11 @@ SET time_zone = "+00:00";
 -- 表的结构 `bg`
 --
 
-CREATE TABLE `bg` (
-  `id` int(11) NOT NULL,
-  `url` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `bg` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 -- --------------------------------------------------------
 
@@ -37,11 +38,12 @@ CREATE TABLE `bg` (
 -- 表的结构 `search`
 --
 
-CREATE TABLE `search` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `search` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
-  `url` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `url` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
 
 -- --------------------------------------------------------
 
@@ -49,12 +51,13 @@ CREATE TABLE `search` (
 -- 表的结构 `site`
 --
 
-CREATE TABLE `site` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `site` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `type_id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
-  `url` text NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `url` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=84 ;
 
 -- --------------------------------------------------------
 
@@ -62,63 +65,12 @@ CREATE TABLE `site` (
 -- 表的结构 `site_type`
 --
 
-CREATE TABLE `site_type` (
-  `id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `site_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `bg`
---
-ALTER TABLE `bg`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `search`
---
-ALTER TABLE `search`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `site`
---
-ALTER TABLE `site`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `site_type`
---
-ALTER TABLE `site_type`
-  ADD PRIMARY KEY (`id`);
-
---
--- 在导出的表使用AUTO_INCREMENT
---
-
---
--- 使用表AUTO_INCREMENT `bg`
---
-ALTER TABLE `bg`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- 使用表AUTO_INCREMENT `search`
---
-ALTER TABLE `search`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- 使用表AUTO_INCREMENT `site`
---
-ALTER TABLE `site`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- 使用表AUTO_INCREMENT `site_type`
---
-ALTER TABLE `site_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
